5b14b2b2813b206a9a7dfe93b563c554c456be97
Vladimir Yazykov <neizerth@gmail.com>
2016-11-01 13:27:43 +0300

    init

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=evernote',
-    'username' => 'root',
-    'password' => '12345',
-    'charset' => 'utf8',
-];
---
+++ config/db.sample.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=evernote;unix_socket=/Applications/AMPPS/var/mysql.sock',
+    'username' => 'root',
+    'password' => 'mysql',
+    'charset' => 'utf8',
+];
